2f9adfb584a006777a277b03709f1c181f0ef099
Joaquin del VAL <delvaljoaquin@gmail.com>
2022-08-02 11:18:45 -0300

    using cache

--- src/v1/routes/workoutRoutes.js
+++ src/v1/routes/workoutRoutes.js
@@ -1,10 +1,16 @@
 const express = require("express");
+const apicache = require("apicache");
+
 const workoutController = require("../../controllers/workoutController");
 const recordController = require("../../controllers/recordController");
 
 const router = express.Router();
 
-router.get("/", workoutController.getAllWorkouts);
+// We can define a new cache by calling apicache.middleware and use it as a middleware inside our get route.
+// Inside there you can define how long your data should be cached. I've chosen two minutes.
+const cache = apicache.middleware;
+
+router.get("/", cache('2 minutes'), workoutController.getAllWorkouts);
 
 router.get("/", workoutController.getAllWorkouts);
 
